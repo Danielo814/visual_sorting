48f7f83d3929f9719c42a70e5e57c9744b6b4cd0
Danielo814 <ojedadaniel92@gmail.com>
2020-03-04 16:31:48 -0800

    changes to settimeout to sort over whole array

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 let elements = document.getElementsByClassName("row");
 
-for (let i = 0; i < elements.length; i++) {
-  startAnimation(elements[i], i);
+function init() {
+  var timer = 0;
+  for (let i = 0; i < elements.length; i++) {
+    for (let j = 0; j < elements.length; j++) {
+      timer = timer + 1;
+      startAnimation(elements[j], j, timer);
+      console.log(timer);
+    }
+  }
 }
 
-function startAnimation(element, index) {
+// for (let i = 0; i < elements.length; i++) {
+//   startAnimation(elements[i], i);
+// }
+
+function startAnimation(element, index, timer) {
+  // if (index === 10) {
+  //   var test = index;
+  // }
+  // test = test + 1;
+  // console.log(test);
   setTimeout(function() {
     if (index > 0) {
       elements[index - 1].classList.remove("red-square");
     }
     let digit = parseInt(element.innerHTML, 10);
+
     let nextDigit = parseInt(elements[index + 1].innerHTML, 10);
+
     if (digit > nextDigit) {
       element.innerHTML = nextDigit;
       elements[index + 1].innerHTML = digit;
     }
     element.classList.add("red-square");
-  }, 1000 * index);
-  element.classList.remove("red-square");
+  }, 1000 * timer);
 }
+
+init();

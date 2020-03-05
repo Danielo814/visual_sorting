d3c8fd506a296c67e80ced5ee1f97923fea4cbe8
Danielo814 <ojedadaniel92@gmail.com>
2020-03-04 20:55:25 -0800

    add some transitioning

--- script.js
+++ script.js
@@ -6,26 +6,33 @@ function init() {
     for (let j = 0; j < elements.length; j++) {
       timer = timer + 1;
       startAnimation(elements[j], j, timer);
-      console.log(timer);
     }
   }
 }
 
 function startAnimation(element, index, timer) {
-  setTimeout(function() {
+  var sort = setTimeout(function() {
+    element.classList.remove("blue-square");
     if (index > 0) {
       elements[index - 1].classList.remove("red-square");
     }
     let digit = parseInt(element.innerHTML, 10);
 
     let nextDigit = parseInt(elements[index + 1].innerHTML, 10);
-
     if (digit > nextDigit) {
+      elements[index + 1].classList.add("blue-square");
       element.innerHTML = nextDigit;
+      element.style.transition = "all 2s";
+      elements[index + 1].style.transition = "all 0.8s";
       elements[index + 1].innerHTML = digit;
     }
     element.classList.add("red-square");
   }, 1000 * timer);
+}
+
+function myStopFunction(sorter) {
+  clearTimeout(sorter);
+  console.log("lol");
 }
 
 init();

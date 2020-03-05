74cc6bc2c8347e8dd9a6dcb129444b1cc2bd87d0
Danielo814 <ojedadaniel92@gmail.com>
2020-03-04 21:08:24 -0800

    add and remove classes for animation

--- script.js
+++ script.js
@@ -15,18 +15,21 @@ function startAnimation(element, index, timer) {
     element.classList.remove("blue-square");
     if (index > 0) {
       elements[index - 1].classList.remove("red-square");
+      elements[index - 1].classList.remove("thick-line");
     }
     let digit = parseInt(element.innerHTML, 10);
 
     let nextDigit = parseInt(elements[index + 1].innerHTML, 10);
     if (digit > nextDigit) {
       elements[index + 1].classList.add("blue-square");
+      element.classList.add("red-square");
       element.innerHTML = nextDigit;
-      element.style.transition = "all 2s";
+      // element.style.transition = "all 0.8s";
       elements[index + 1].style.transition = "all 0.8s";
       elements[index + 1].innerHTML = digit;
     }
-    element.classList.add("red-square");
+    element.classList.add("thick-line");
+    // element.classList.add("red-square");
   }, 1000 * timer);
 }
 
